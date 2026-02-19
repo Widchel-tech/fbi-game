427fe25540f96d9984310380221ceb6114090b90
Widchel-tech <widchel.ant008@gmail.com>
2026-02-19 17:50:04 -0600

    Add game.js

---
+++ game.js
@@ -0,0 +1,9 @@
+function choice1() {
+    document.getElementById("story").innerText =
+        "The victim has a strange symbol carved into their arm. This was planned.";
+}
+
+function choice2() {
+    document.getElementById("story").innerText =
+        "You find a hidden safe behind a painting. It's locked.";
+}
